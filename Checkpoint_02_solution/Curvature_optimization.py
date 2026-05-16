11ce1ad377fdc86b7342366930163a8b12a95529
HarshitaSinha3116 <225584548+HarshitaSinha3116@users.noreply.github.com>
2026-05-16 01:23:36 -0400

    Update and rename Curvature_optimization to Curvature_optimization.py

--- Checkpoint_02_solution/Curvature_optimization.py
+++ Checkpoint_02_solution/Curvature_optimization.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-eta = 0.01
-
 def main(args=None):
+    eta = 0.01
     x = np.array([1, 2, 3, 4])
-    y = np.array([1, 3, 2, 3])
+    y = np.array([1, 3, 2, 3], dtype = float)
     A = np.zeros((12, 12))
     B = np.zeros(12)
     eqn = 0
@@ -22,6 +21,7 @@ def main(args=None):
         eqn += 1
     for i in range(1,3):
         dx = x[i] - x[i-1]
+
         A[eqn, 4*(i-1)+1] = 1
         A[eqn, 4*(i-1)+2] = 2*dx
         A[eqn, 4*(i-1)+3] = 3*dx**2
@@ -54,22 +54,27 @@ def main(args=None):
         yi_vals = computePoly(i, xi_vals)
         X.extend(xi_vals)
         Y.extend(yi_vals)
-    
-    def firstDerivative(i, xi):
-        a = coeffs[4*i]
-        b = coeffs[4*i+1]
-        c = coeffs[4*i+2]
-        d = coeffs[4*i+3]
-        return b + 2*c*(xi - x[i]) + 3*d*(xi - x[i])**2
-    
-    def secondDerivative(i, xi):
-        a = coeffs[4*i]
-        b = coeffs[4*i+1]
-        c = coeffs[4*i+2]
-        d = coeffs[4*i+3]
-        return 2*c + 6*d*(xi - x[i])
  
-    def computeCurvature():
+    def computeCurvature(y_input):
+      
+      B_temp = np.zeros(12)
+
+      eqn = 0
+
+      for i in range(3):
+
+        B_temp[eqn] = y_input[i]
+        eqn += 1
+
+        B_temp[eqn] = y_input[i+1]
+        eqn += 1
+
+      B_temp[eqn] = 0
+      eqn += 1
+      B_temp[eqn] = 0
+
+      coeffs_temp = np.linalg.solve(A, B_temp)
+
       total_curvature = 0
 
       for i in range(3):
@@ -78,8 +83,12 @@ def main(args=None):
 
         for xi in xi_vals:
 
-            yd = firstDerivative(i, xi)
-            y2d = secondDerivative(i, xi)
+            b = coeffs_temp[4*i+1]
+            c = coeffs_temp[4*i+2]
+            d = coeffs_temp[4*i+3]
+
+            yd = b + 2*c*(xi - x[i]) + 3*d*(xi - x[i])**2
+            y2d = 2*c + 6*d*(xi - x[i])
 
             kappa = abs(y2d)/(1 + yd**2)**1.5
 
@@ -87,17 +96,38 @@ def main(args=None):
 
       return total_curvature
       
-    J = computeCurvature()
-     
-    y_opt = y.copy()
+    J = computeCurvature(y)
+    
+    def computeGradient(index):
+      eps = 0.000001
+      J1 = computeCurvature(y)
+      y_temp = y.copy()
+      y_temp[index] += eps
+      J2 = computeCurvature(y_temp)
+      
+      print(J1)
+      print(J2)
+      print(J2 - J1)
+
+      gradient = (J2 - J1)/eps
+      return gradient
  
-    y_opt[1] -= eta
-    y_opt[2] += eta
+    grad1 = computeGradient(1)
+    grad2 = computeGradient(2)
+    
+    print(grad1)
+    print(" ")
+    print(grad2)
+    print("\n")
+   
+    y_opt = y.copy()
+
+    y_opt[1] -= eta*grad1
+    y_opt[2] -= eta*grad2
 
     B_opt = np.zeros(12)
 
     eqn = 0
-
     for i in range(3):
 
       B_opt[eqn] = y_opt[i]
@@ -121,14 +151,13 @@ def main(args=None):
        d = coeffs_opt[4*i+3]
 
        yi_vals = (
-        y_opt[i]
+        a
         + b*(xi_vals-x[i])
         + c*(xi_vals-x[i])**2
         + d*(xi_vals-x[i])**3
-    )
+)
        X_opt.extend(xi_vals)
        Y_opt.extend(yi_vals)
-
 
     plt.plot(X, Y, label="Original Spline")
     plt.plot(X_opt, Y_opt, label="Optimized Spline")
@@ -136,3 +165,4 @@ def main(args=None):
     plt.scatter(x, y_opt, color='red')
     plt.legend()
     plt.show()
+
