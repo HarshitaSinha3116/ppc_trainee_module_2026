51be09e26549feb56aa40e66d78b6af33fc6042e
HarshitaSinha3116 <225584548+HarshitaSinha3116@users.noreply.github.com>
2026-05-16 01:25:29 -0400

    Update and rename Curvature_optimization_of_csv_using_libraries to Curvature_optimization_of_csv_using_libraries.py

--- Checkpoint_02_solution/Curvature_optimization_of_csv_using_libraries.py
+++ Checkpoint_02_solution/Curvature_optimization_of_csv_using_libraries.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import splprep, splev
+from scipy.optimize import approx_fprime  
 import os
 
 def main(args=None):
@@ -19,19 +20,27 @@ def main(args=None):
     tck, u = splprep([x, y], s=0, per=True)
     u_new = np.linspace(0, 1, 100)
     x_smooth, y_smooth = splev(u_new, tck)
+    
+    def computeCurvature(y_opt):
+      tck_, _ = splprep([x, y_opt], s=0, per=True)
+      dx, dy = splev(u_new, tck_, der=1)
+      d2x, d2y = splev(u_new, tck_, der=2)
 
-    dx, dy = splev(u_new, tck, der=1)
-    d2x, d2y = splev(u_new, tck, der=2)
+      kappa = np.abs(dx*d2y - dy*d2x) / (dx**2 + dy**2)**1.5
+      J = np.sum(kappa**2)
+      return J
+    
+    y_opt = y.copy()#considerng non-endpoints
+    eps = 0.00001
 
-    kappa = np.abs(dx*d2y - dy*d2x) / (dx**2 + dy**2)**1.5
-    J = np.sum(kappa**2)
+    gradient = approx_fprime(y_opt, computeCurvature, eps)
 
-    y_opt = y.copy()
-    y_opt[1:-1] -= y_opt[1:-1] * eta
+    y_opt -= eta*gradient
+    y_opt[0] = y[0]
+    y_opt[-1] = y[-1]
 
-    tck_opt, u_opt = splprep([x, y_opt], s=0)
+    tck_opt, u_opt = splprep([x, y_opt], s=0, per = True)
     x_opt_smooth, y_opt_smooth = splev(u_new, tck_opt)
-
 
     plt.figure(figsize=(6, 6))
     plt.plot(x_smooth, y_smooth, label='Cubic Spline', linewidth=2)
@@ -45,4 +54,6 @@ def main(args=None):
     plt.grid()
     plt.axis('equal')
     plt.show()
+
+
 
